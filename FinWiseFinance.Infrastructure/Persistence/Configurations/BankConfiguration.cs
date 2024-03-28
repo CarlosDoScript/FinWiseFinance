2f6ef1585ff95516760f590ff6f160c5ccc7558e
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-03-27 22:57:50 -0300

    adicionando o bankConfiguration para a configuracao da tabela

---
+++ FinWiseFinance.Infrastructure/Persistence/Configurations/BankConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using FinWiseFinance.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinWiseFinance.Infrastructure.Persistence.Configurations
+{
+    public class BankConfiguration : IEntityTypeConfiguration<Bank>
+    {
+        public void Configure(EntityTypeBuilder<Bank> builder)
+        {
+            builder
+                .ToTable(nameof(Bank));
+
+            builder
+                .HasKey(b => b.Id);
+
+            builder
+                .Property(b => b.Title)
+                .IsRequired()
+                .HasColumnName("Title")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(255);
+
+            builder
+               .Property(b => b.Description)
+               .IsRequired()
+               .HasColumnName("Title")
+               .HasColumnType("VARCHAR")
+               .HasMaxLength(255);
+
+            builder.Property(b => b.Active);
+        }
+    }
+}
