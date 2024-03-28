662f43094dbd56d4ef4df5cae3bd3da37a534602
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-03-27 22:34:40 -0300

    criando o dbContext, configurando os db set e tambem adicionando a connection string no appSettings json

---
+++ FinWiseFinance.Infrastructure/Persistence/FinWiseFinanceDbContext.cs
@@ -0,0 +1,32 @@
+﻿using FinWiseFinance.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace FinWiseFinance.Infrastructure.Persistence
+{
+    public class FinWiseFinanceDbContext : DbContext
+    {
+        public FinWiseFinanceDbContext(DbContextOptions<FinWiseFinanceDbContext> options) : base(options)
+        {
+        }
+
+        #region DbSet
+        public DbSet<Bank> Banks { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<CompanyBranch> CompanyBranches { get; set; }
+        public DbSet<DailyExpense> DailyExpenses { get; set; }
+        public DbSet<InstallmentBill> InstallmentBills { get; set; }
+        public DbSet<Profession> Professions { get; set; }
+        public DbSet<PurchaseObjectiveHistory> PurchaseObjectiveHistories { get; set; }
+        public DbSet<PurchaseTarget> PurchaseTargets { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserProfession> UserProfessions { get; set; }
+        #endregion
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+    }
+}
