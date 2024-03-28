a448aaac650d1c7a86f99016ab05bc383ccb81d1
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-03-28 10:16:40 -0300

    Adicionando CompanyBranchConfiguration

--- FinWiseFinance.CORE/Entities/CompanyBranch.cs
+++ FinWiseFinance.CORE/Entities/CompanyBranch.cs
@@ -2,14 +2,16 @@
 {
     public class CompanyBranch : BaseEntity
     {
-        public CompanyBranch(string description)
+        public CompanyBranch(string title,string description)
         {
+            Title = title;
             Description = description;
 
             Active = true;
             CreatedAt = DateTime.Now;
         }
 
+        public string Title { get; private set; }
         public string Description { get; private set; }
         public DateTime CreatedAt { get; private set; }
         public bool Active { get; private set; }
---
+++ FinWiseFinance.Infrastructure/Persistence/Configurations/CompanyBranchConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using FinWiseFinance.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinWiseFinance.Infrastructure.Persistence.Configurations
+{
+    public class CompanyBranchConfiguration : IEntityTypeConfiguration<CompanyBranch>
+    {
+        public void Configure(EntityTypeBuilder<CompanyBranch> builder)
+        {
+            builder
+                .ToTable(nameof(CompanyBranch));
+
+            builder
+                .HasKey(c => c.Id);
+
+            builder
+                .Property(c => c.Title)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(255);
+
+            builder
+               .Property(c => c.Description)
+               .HasColumnType("VARCHAR")
+               .HasMaxLength(255);
+
+            builder
+               .Property(b => b.CreatedAt)
+               .HasColumnType("DATETIME");
+        }
+    }
+}
