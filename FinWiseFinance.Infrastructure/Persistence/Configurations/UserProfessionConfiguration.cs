e428effda752410d592e65814e54de2b407ca5b9
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-03-31 00:33:04 -0300

    criando o UserProfessionConfiguration e colocando a relação de muitos para muitos com uma classe intermediaria

---
+++ FinWiseFinance.Infrastructure/Persistence/Configurations/UserProfessionConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using FinWiseFinance.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinWiseFinance.Infrastructure.Persistence.Configurations
+{
+    public class UserProfessionConfiguration : IEntityTypeConfiguration<UserProfession>
+    {
+        public void Configure(EntityTypeBuilder<UserProfession> builder)
+        {
+            builder
+                .ToTable(nameof(UserProfession));
+
+            builder 
+                .HasKey(x => x.Id);
+
+            builder
+                .Property(u => u.CreatedAt)
+                .HasColumnType("DATETIME");
+
+
+            // RELAÇÃO DE MUITOS PARA MUITOS
+            builder
+                .HasOne(u => u.User)
+                .WithMany(up => up.UserProfessions)
+                .HasForeignKey(u => u.IdUser)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+                .HasOne(u => u.Profession)
+                .WithMany(up => up.UserProfessions)
+                .HasForeignKey(u => u.IdProfession)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
