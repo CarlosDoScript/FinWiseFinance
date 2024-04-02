411cc26b4265edf891aaff97eae4c6b2b4b69bb9
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-04-01 22:41:28 -0300

    criando a interface de IAuthService e implementando o hash da senha e a geração do JWT Token para o login do usuário

---
+++ FinWiseFinance.CORE/Services/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿namespace FinWiseFinance.Core.Services
+{
+    public interface IAuthService 
+    {
+        string GenerateJwtToken(string fullName, string email);
+        string ComputeSha256Hash(string password);
+    }
+}
