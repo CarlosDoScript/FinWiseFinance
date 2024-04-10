2f1204ce36caf9a8cbb237e8960b08673f296bb1
Carlos Henrique <carlosmvsep3@hotmail.com>
2024-04-10 00:04:43 -0300

    criando a validação da criação do usuário falta alguns campos

--- FinWiseFinance.Application/Validators/CreateUserCommandValidator.cs
+++ FinWiseFinance.Application/Validators/CreateUserCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FinWiseFinance.Application.Commands.CreateUser;
+using FinWiseFinance.Application.Validators.Exthension;
 using FluentValidation;
 
 namespace FinWiseFinance.Application.Validators
@@ -7,7 +8,36 @@ namespace FinWiseFinance.Application.Validators
     {
         public CreateUserCommandValidator()
         {
-            
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .MaximumLength(50)
+                .WithMessage("Tamanho máximo do primeiro nome é de 50 caracteres")
+                .NotNull()
+                .WithMessage("Primeiro nome obrigatório") 
+                .NotEmpty()
+                .WithMessage("Primeiro nome obrigatório");
+
+
+            RuleFor(x => x.LastName)
+                .MaximumLength(50)
+                .WithMessage("Tamanho máximo do último nome é de 50 caracteres")
+                .NotNull()
+                .WithMessage("Último nome obrigatório")
+                .NotEmpty()
+                .WithMessage("Último nome obrigatório");
+
+            RuleFor(x => x.Email)
+                 .EmailAddress()
+                .WithMessage("E-mail não válido");
+
+            RuleFor(x => x.PhoneNumber)
+                .MaximumLength(15)
+                .WithMessage("Número máximo de dígitos para telefone é 15");
+
+            RuleFor(x => x.CpfCnpj)
+                .NotEmpty().WithMessage("CPF/CNPJ obrigatório")
+                .Must(ExthensionMethodValidator.IsValidCpfOrCnpj)
+                .WithMessage("CPF/CNPJ inválido");          
         }
     }
 }
